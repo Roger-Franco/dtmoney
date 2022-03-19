9317216d861cdf9ee24c160c320fcb1691ec93a3
Roger-Franco <rogerfrancogalt@gmail.com>
2022-03-18 22:30:25 -0400

    Componente TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import Summary from '../Summary'
+import TransactionsTables from '../TransactionsTable'
 import { Container } from './styles'
 
 export default function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTables />
     </Container>
   )
 }
