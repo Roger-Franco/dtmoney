dfe32ab576b62bb476704f4dd5795111b7b8a04a
Roger-Franco <rogerfrancogalt@gmail.com>
2022-04-15 15:17:51 -0400

    Context API no React

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root') // orientaçao do modal visando acessibilidade
 
@@ -18,13 +19,13 @@ export default function App() {
     setisNewTransactionOpen(false)
   }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
       <NewTransactionModal isOpen={isNewTransactionOpen} onRequestClose={handleCloseNewTransactionModal} />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
