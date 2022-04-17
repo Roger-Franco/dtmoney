fbe477952d33e367665551e721fba6297296bdea
Roger-Franco <rogerfrancogalt@gmail.com>
2022-04-17 00:39:07 -0400

    Calculando resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,7 +9,28 @@ import { Container } from './styles';
 export default function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
-  console.log(transactions, 'transactions');
+  // const totalDeposits = transactions.reduce((acc, transaction) => {
+  //   if (transaction.type === 'deposit') {
+  //     return acc + transaction.amount
+  //   }
+  //   return acc
+  // }, 0)
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount
+      acc.total += transaction.amount
+    } else {
+      acc.withdraws += transaction.amount
+      acc.total -= transaction.amount
+
+    }
+    return acc
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+  })
 
 
 
@@ -21,21 +42,30 @@ export default function Summary() {
           <p>Entradas</p>
           <img src={incomeImage} alt="Entradas" />
         </header>
-        <strong>R$ 1000,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.deposits)}</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImage} alt="Saídas" />
         </header>
-        <strong> - R$ 500,00</strong>
+        <strong> - {new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.withdraws)}</strong>
       </div>
       <div className='highlight-background'>
         <header>
           <p>Toral</p>
           <img src={totalImage} alt="Total" />
         </header>
-        <strong>R$ 500,00</strong>
+        <strong>{new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(summary.total)}</strong>
       </div>
     </Container>
   )
